3b6abe1ed9c8da277886a0c5b0cc61818bf3d608
unknown <nituldeori63@gmail.com>
2020-12-28 19:42:40 +0530

    Added: Ability to calculate wages for a month assuming 20 working days

--- EmployeeWageCalculation.js
+++ EmployeeWageCalculation.js
@@ -43,11 +43,22 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
-let empHrs = 0;
+/*let empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10) %3;
 empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: "+empWage);
+*/
+
+//UC 4
+const NUM_OF_WORKING_DAYS = 20;
+let empHrs = 0;
+for (let day = 0; day<NUM_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random() * 10) %3;
+    empHrs += getWorkingHours(empCheck);
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total Hrs: "+empHrs+" Emp Wage: "+empWage);
 /*
 function calcDailyWage(empHrs){
     return empHrs * WAGE_PER_HOUR;
