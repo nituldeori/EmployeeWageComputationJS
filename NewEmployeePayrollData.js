f84c096dc3e632d35607bc6d70c18dd90febea6d
unknown <nituldeori63@gmail.com>
2020-12-30 01:54:30 +0530

    Added: Ability to check employee id and salary>0, gender is M or  and date is not a future date

--- NewEmployeePayrollData.js
+++ NewEmployeePayrollData.js
@@ -15,16 +15,22 @@ class NewEmployeePayrollData{
         this.startDate = params[4];
     }
 
-    get name(){ return this._name;}
+    get name(){ return this.name;}
+    get id(){return this.id;}
+    get salary(){return this.salary;}
+    get gender(){return this.gender;}
+    get startDate(){return this.startDate;}
+    
     set setName(name) {
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
         if(nameRegex.test(name)){
-            this._name = name;
+            this.name = name;
 
         }
             
-        else
+        else{
             throw 'Name is Incorrect!';
+        }
     }
 
     set setId(id){
@@ -37,7 +43,37 @@ class NewEmployeePayrollData{
         }
     }
 
-    
+    set setSalary(salary){
+        let salaryRegex = RegExp('^[1-9]{1}[0-9]*');
+        if(salaryRegex.test(salary)){
+            this.salary = salary;
+        }
+        else{
+            throw 'Salary should be non-zero positive integer';
+        }
+    }
+
+    set setGender(gender){
+        let genderRegex = RegExp('^(M || F)');
+        if(genderRegex.test(gender)){
+            this.gender = gender;
+        }
+        else{
+            throw 'Gender should be M or F';
+        }
+    }
+
+    set setStartDate(startDate){
+        let givenDate = new Date(startDate);
+        let todayDate = new Date();
+        if(givenDate > todayDate){
+            throw 'Start Date cannot be a future date'
+        }
+        else{
+            this.startDate = startDate;
+        }
+
+    }
 
     toString(){
         const options = {year: 'numeric', month: 'long', day: 'numeric'};
@@ -51,9 +87,14 @@ class NewEmployeePayrollData{
     
 let employeePayrollData = new NewEmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
+
 try{
-    employeePayrollData.setName = 'Jo';
-    console.log(employeePayrollData.toString());
+    employeePayrollData.setId='1';
+    employeePayrollData.setName='Jon';
+    employeePayrollData.setSalary=1999;
+    employeePayrollData.setGender = 'M';
+    employeePayrollData.setStartDate='2019-10-11';
+    console.log("id: "+employeePayrollData.id+" name: "+employeePayrollData.name+ " salary: "+employeePayrollData.salary+" gender: "+employeePayrollData.gender+" startDate: "+employeePayrollData.startDate);
 } catch(e){
     console.error(e);
 }
